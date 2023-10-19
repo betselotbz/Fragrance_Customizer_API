24acceabf5f6c3a4e54539e0ef3586848c9d4e85
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-19 16:48:59 -0500

    Add constructors, getters and setters for Perfume Class

--- src/main/java/com/example/fragrance_customizer_api/model/Perfume.java
+++ src/main/java/com/example/fragrance_customizer_api/model/Perfume.java
@@ -20,6 +20,51 @@ public class Perfume {
     @Column (nullable = false)
     private String Image;
 
+    public Perfume(Long id, String name, String family, String notes, String image) {
+        this.id = id;
+        this.name = name;
+        Family = family;
+        Notes = notes;
+        Image = image;
+    }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFamily() {
+        return Family;
+    }
+
+    public void setFamily(String family) {
+        Family = family;
+    }
+
+    public String getNotes() {
+        return Notes;
+    }
+
+    public void setNotes(String notes) {
+        Notes = notes;
+    }
+
+    public String getImage() {
+        return Image;
+    }
+
+    public void setImage(String image) {
+        Image = image;
+    }
 }
