d01a41cb7951950e149d77212b4a3bcf5ae18b26
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-19 15:46:43 -0500

    fix getter and setter in User class

--- src/main/java/com/example/fragrance_customizer_api/model/User.java
+++ src/main/java/com/example/fragrance_customizer_api/model/User.java
@@ -35,22 +35,35 @@ public class User {
 
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
-    public void setName() {
+
+    public void setName(String name) {
         this.name = name;
     }
+
     public String getEmailAddress() {
         return emailAddress;
     }
-    public void setEmailAddress() {
+
+    public void setEmailAddress(String emailAddress) {
         this.emailAddress = emailAddress;
     }
+
     public String getPassword() {
         return password;
     }
-    public void setPassword() {
+
+    public void setPassword(String password) {
         this.password = password;
     }
 
--- src/main/java/com/example/fragrance_customizer_api/model/UserCartItem.java
+++ src/main/java/com/example/fragrance_customizer_api/model/UserCartItem.java
@@ -29,6 +29,6 @@ public class UserCartItem {
     public UserCartItem() {
 
     }
-
+    public
 
 }
