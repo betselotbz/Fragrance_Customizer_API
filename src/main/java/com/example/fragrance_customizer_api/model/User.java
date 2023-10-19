0c3c2bfe381481dafbd8db36a2fc2ca51de4d9ca
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-19 14:18:05 -0500

    Add constructor to User class

--- src/main/java/com/example/fragrance_customizer_api/model/User.java
+++ src/main/java/com/example/fragrance_customizer_api/model/User.java
@@ -4,6 +4,9 @@ package com.example.fragrance_customizer_api.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Entity
 @Table(name = "users")
@@ -18,7 +21,16 @@ public class User {
     private String emailAddress;
     @Column
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) // ensure that the password is not accidentally exposed in the JSON representation of the object
-    private String passWord;
+    private String password;
 
+    public User() {
+
+    }
+    public User(Long id, String name, String emailAddress, String password) {
+        this.id = id;
+        this.name = name;
+        this.emailAddress=emailAddress;
+        this.password=password;
+    }
 
 }
