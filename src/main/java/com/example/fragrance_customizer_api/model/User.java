55475f22649bc09a7f0a1934e86884f53822ca40
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-19 12:58:12 -0500

    Add annotations to User class

--- src/main/java/com/example/fragrance_customizer_api/model/User.java
+++ src/main/java/com/example/fragrance_customizer_api/model/User.java
@@ -1,6 +1,22 @@
 package com.example.fragrance_customizer_api.model;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String name;
+    @Column(unique = true) //no two records in the table can have the same emailAddress.
+    private String emailAddress;
+    @Column
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) // ensure that the password is not accidentally exposed in the JSON representation of the object
+    private String passWord;
 }
