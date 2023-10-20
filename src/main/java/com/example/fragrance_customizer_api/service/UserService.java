bdbda409e97444a5a5306e06b8374437cfba123e
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-20 18:56:08 -0500

    Implement user creation logic om UserService

--- src/main/java/com/example/fragrance_customizer_api/service/UserService.java
+++ src/main/java/com/example/fragrance_customizer_api/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.fragrance_customizer_api.service;
 
+import com.example.fragrance_customizer_api.exception.InformationExistException;
 import com.example.fragrance_customizer_api.model.User;
 import com.example.fragrance_customizer_api.repository.UserRepository;
 import org.springframework.context.annotation.Lazy;
@@ -15,7 +16,21 @@ public class UserService {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
-
+    public User createUser(User userObject) {
+        if (userObject == null) {
+            throw new IllegalArgumentException("User object cannot be null.");
+        }
+        // Check if a user with the same email address already exists
+        if (!userRepository.existsByEmailAddress(userObject.getEmailAddress())) {
+            // Encode the user's password for security
+            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
+            // Save the user to the database
+            return userRepository.save(userObject);
+        } else {
+            // Throw an exception if the email address is already in use
+            throw new InformationExistException("User with email address " + userObject.getEmailAddress() + " already exists");
+        }
+    }
 
 
 }
