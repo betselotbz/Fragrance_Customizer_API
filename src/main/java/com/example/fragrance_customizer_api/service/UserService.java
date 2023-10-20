62af6f47b5e0a5f264b5d77b740fa421335af816
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-20 14:13:02 -0500

    Create a UserService Class

---
+++ src/main/java/com/example/fragrance_customizer_api/service/UserService.java
@@ -0,0 +1,9 @@
+package com.example.fragrance_customizer_api.service;
+
+import com.example.fragrance_customizer_api.repository.UserRepository;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class UserService {
+
+}
