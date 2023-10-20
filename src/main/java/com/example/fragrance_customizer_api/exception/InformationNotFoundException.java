f96067e83f5d37196209414ec4349eafa923ddf2
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-20 14:49:03 -0500

    Create constructor for InformationNotFoundException Class

--- src/main/java/com/example/fragrance_customizer_api/exception/InformationNotFoundException.java
+++ src/main/java/com/example/fragrance_customizer_api/exception/InformationNotFoundException.java
@@ -1,5 +1,18 @@
 package com.example.fragrance_customizer_api.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class InformationNotFoundException extends RuntimeException{
 
+    /*
+     * Constructor for InformationNotFoundException.
+     *
+     * @param message A message describing the specific reason for the exception
+     */
+
+    public InformationNotFoundException(String message) { //constructor tha accepts message
+        super(message); //means this runtimeexception
+    }
 }
