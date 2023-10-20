89b35cd640149a563f1fc57ddc9bf75e3345d5b0
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-19 23:03:41 -0500

    Add annotations to CustomFragrance Class

--- src/main/java/com/example/fragrance_customizer_api/model/CustomFragrance.java
+++ src/main/java/com/example/fragrance_customizer_api/model/CustomFragrance.java
@@ -9,6 +9,15 @@ public class CustomFragrance {
     @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_cart_item_id")
+    private UserCartItem userCartItem;
+    @Column
+    private String Family;
+    @Column
+    private String Notes;
+
+
 
 
 }
