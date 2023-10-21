4942f73b9ff5f7b53d4f450d1b9c84ec06d59a79
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-21 17:38:08 -0500

    Add Cucumber and Spring Boot configuration to UserCartItemTestDefs

--- src/test/java/definitions/UserCartItemTestDefs.java
+++ src/test/java/definitions/UserCartItemTestDefs.java
@@ -1,8 +1,23 @@
 package definitions;
 
+import com.example.fragrance_customizer_api.FragranceCustomizerApiApplication;
 import io.cucumber.java.en.Given;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
+@CucumberContextConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = FragranceCustomizerApiApplication.class)
 public class UserCartItemTestDefs {
+
+    private final Logger logger = LoggerFactory.getLogger(UserCartItemTestDefs.class);
+    private static final String Base_URL = "http://localhost:";
+
+    @LocalServerPort
+    String port;
+
     @Given("a user with ID {int} exists")
     public void aUserWithIDExists(int arg0) {
     }
