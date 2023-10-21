249a9a2fce4003a20376b32fcff28c6295765ea7
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-20 19:29:34 -0500

    Update MyUserDetailsService to use emailAddress parameter in loadUserByUsername

--- src/main/java/com/example/fragrance_customizer_api/security/MyUserDetailsService.java
+++ src/main/java/com/example/fragrance_customizer_api/security/MyUserDetailsService.java
@@ -2,11 +2,19 @@ package com.example.fragrance_customizer_api.security;
 
 import com.example.fragrance_customizer_api.model.User;
 import com.example.fragrance_customizer_api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-//To retrieve username and password
+@Service
 public class MyUserDetailsService implements UserDetailsService {
+    private UserService userService;
 
+    @Override
+    public UserDetails loadUserByUsername(String emailAddress) throws UsernameNotFoundException {
+        User user = userService.findUserByEmailAddress(emailAddress);
+        return new MyUserDetails(user);
+    }
 }
