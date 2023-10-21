bb07c7028ba165d4fa08bdcdea7436268ba6c9cb
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-20 22:21:08 -0500

    Added JWT property to LoginResponse class to encapsulate JWT tokens

--- src/main/java/com/example/fragrance_customizer_api/model/response/LoginResponse.java
+++ src/main/java/com/example/fragrance_customizer_api/model/response/LoginResponse.java
@@ -1,4 +1,17 @@
 package com.example.fragrance_customizer_api.model.response;
 
 public class LoginResponse {
+    private String jwt;
+
+    public LoginResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
 }
