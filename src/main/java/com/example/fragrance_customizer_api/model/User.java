a8c21f31ab7f711323118871f41bd2b259aa78d5
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-21 13:27:45 -0500

    Add Java Docstrings to User and UserCartItem Class

--- src/main/java/com/example/fragrance_customizer_api/model/User.java
+++ src/main/java/com/example/fragrance_customizer_api/model/User.java
@@ -1,7 +1,5 @@
 package com.example.fragrance_customizer_api.model;
 
-
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.annotations.LazyCollection;
 import org.hibernate.annotations.LazyCollectionOption;
@@ -9,8 +7,9 @@ import org.hibernate.annotations.LazyCollectionOption;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-
-
+/**
+ * Entity class representing a user in the application.
+ */
 @Entity
 @Table(name = "users")
 public class User {
@@ -20,7 +19,7 @@ public class User {
     private Long id;
     @Column
     private String name;
-    @Column(unique = true) //no two records in the table can have the same emailAddress.
+    @Column(unique = true) //No two records in the table can have the same emailAddress.
     private String emailAddress;
     @Column
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) // ensure that the password is not accidentally exposed in the JSON representation of the object
@@ -30,54 +29,113 @@ public class User {
     @LazyCollection(LazyCollectionOption.FALSE)
     private List<UserCartItem> userCartItems = new ArrayList<>();
 
+    /**
+     * Default constructor needed for JPA.
+     */
     public User() {
 
     }
+    /**
+     * Constructor to initialize a User with specific values.
+     *
+     * @param id           The ID of the user.
+     * @param name         The name of the user.
+     * @param emailAddress The email address of the user.
+     * @param password     The password of the user.
+     */
     public User(Long id, String name, String emailAddress, String password) {
         this.id = id;
         this.name = name;
         this.emailAddress=emailAddress;
         this.password=password;
     }
-
+    /**
+     * Get the ID of the user.
+     *
+     * @return The ID of the user.
+     */
     public Long getId() {
         return id;
     }
-
+    /**
+     * Set the ID of the user.
+     *
+     * @param id The ID to set.
+     */
     public void setId(Long id) {
         this.id = id;
     }
-
+    /**
+     * Get the name of the user.
+     *
+     * @return The name of the user.
+     */
     public String getName() {
         return name;
     }
-
+    /**
+     * Set the name of the user.
+     *
+     * @param name The name to set.
+     */
     public void setName(String name) {
         this.name = name;
     }
-
+    /**
+     * Get the email address of the user.
+     *
+     * @return The email address of the user.
+     */
     public String getEmailAddress() {
         return emailAddress;
     }
-
+    /**
+     * Set the email address of the user.
+     *
+     * @param emailAddress The email address to set.
+     */
     public void setEmailAddress(String emailAddress) {
         this.emailAddress = emailAddress;
     }
-
+    /**
+     * Get the password of the user.
+     *
+     * @return The password of the user.
+     */
     public String getPassword() {
         return password;
     }
-
+    /**
+     * Set the password of the user.
+     *
+     * @param password The password to set.
+     */
     public void setPassword(String password) {
         this.password = password;
     }
+    /**
+     * Get the list of cart items associated with the user.
+     *
+     * @return The list of cart items associated with the user.
+     */
     public List<UserCartItem> getUserCartItems() {
         return userCartItems;
     }
+    /**
+     * Set the list of cart items associated with the user.
+     *
+     * @param userCartItems The list of cart items to set.
+     */
     public void setUserCartItems(List<UserCartItem> userCartItems) {
         this.userCartItems = userCartItems;
     }
-
+    /**
+     * Returns a string representation of the User object.
+     *
+     * This implementation includes the user's id, name, email address, and password.
+     *
+     * @return A string representation of the User.
+     */
     @Override
     public String toString() {
         return "User{" +
--- src/main/java/com/example/fragrance_customizer_api/model/UserCartItem.java
+++ src/main/java/com/example/fragrance_customizer_api/model/UserCartItem.java
@@ -1,7 +1,10 @@
 package com.example.fragrance_customizer_api.model;
 
 import javax.persistence.*;
-
+/**
+ * Entity class representing a user's cart item.
+ * Each item in the cart corresponds to a perfume or a custom fragrance.
+ */
 @Entity
 @Table(name = "user_cart_item")
 public class UserCartItem {
@@ -19,49 +22,93 @@ public class UserCartItem {
     @ManyToOne
     @JoinColumn(name = "customFragrance_id")
     private CustomFragrance customFragrance;
-
+    /**
+     * Default constructor required for JPA.
+     */
     public UserCartItem() {
 
     }
-
+    /**
+     * Constructor to initialize a UserCartItem with specific values.
+     *
+     * @param id      The ID of the cart item.
+     * @param user    The user associated with the cart item.
+     * @param perfume The perfume associated with the cart item.
+     */
     public UserCartItem(Long id, User user, Perfume perfume){
         this.id = id;
         this.user = user;
         this.perfume = perfume;
     }
-
+    /**
+     * Get the ID of the cart item.
+     *
+     * @return The ID of the cart item.
+     */
     public Long getId() {
         return id;
     }
-
+    /**
+     * Set the ID of the cart item.
+     *
+     * @param id The ID to set.
+     */
     public void setId(Long id) {
         this.id = id;
     }
-
+    /**
+     * Get the user associated with the cart item.
+     *
+     * @return The user associated with the cart item.
+     */
     public User getUser() {
         return user;
     }
-
+    /**
+     * Set the user associated with the cart item.
+     *
+     * @param user The user to set.
+     */
     public void setUser(User user) {
         this.user = user;
     }
-
+    /**
+     * Get the perfume associated with the cart item.
+     *
+     * @return The perfume associated with the cart item.
+     */
     public Perfume getPerfume() {
         return perfume;
     }
-
+    /**
+     * Set the perfume associated with the cart item.
+     *
+     * @param perfume The perfume to set.
+     */
     public void setPerfume(Perfume perfume) {
         this.perfume = perfume;
     }
-
+    /**
+     * Get the custom fragrance associated with the cart item.
+     *
+     * @return The custom fragrance associated with the cart item.
+     */
     public CustomFragrance getCustomFragrance() {
         return customFragrance;
     }
-
+    /**
+     * Set the custom fragrance associated with the cart item.
+     *
+     * @param customFragrance The custom fragrance to set.
+     */
     public void setCustomFragrance(CustomFragrance customFragrance) {
         this.customFragrance = customFragrance;
     }
-
+    /**
+     * Override of the default `toString` method to provide a string representation of the cart item.
+     *
+     * @return A string representation of the cart item.
+     */
     @Override
     public String toString() {
         return "UserCartItem{" +
