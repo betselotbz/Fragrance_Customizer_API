09df151d576f4ec5784c97c2af6425caf549174a
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-21 17:00:09 -0500

    Create UserCartItemTestDefs

---
+++ src/test/java/definitions/UserCartItemTestDefs.java
@@ -0,0 +1,9 @@
+package definitions;
+
+import io.cucumber.java.en.Given;
+
+public class UserCartItemTestDefs {
+    @Given("a user with ID {int} exists")
+    public void aUserWithIDExists(int arg0) {
+    }
+}
