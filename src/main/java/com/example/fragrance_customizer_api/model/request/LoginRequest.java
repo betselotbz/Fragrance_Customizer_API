917df600a19760ebf7d3782f6d4e7378edde18d0
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-20 19:43:05 -0500

    Add email and password fields with getters to LoginRequest class

--- src/main/java/com/example/fragrance_customizer_api/model/request/LoginRequest.java
+++ src/main/java/com/example/fragrance_customizer_api/model/request/LoginRequest.java
@@ -1,4 +1,13 @@
 package com.example.fragrance_customizer_api.model.request;
 
 public class LoginRequest {
+    private String emailAddress;
+    private String password;
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+    public String getPassword() {
+        return password;
+    }
 }
