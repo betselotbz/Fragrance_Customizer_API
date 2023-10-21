99752be9e0731e0cfadc2141d5047126b013cda1
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-20 20:27:37 -0500

    Configure JWT secret and expiration in JWTUtils class

--- src/main/java/com/example/fragrance_customizer_api/security/JWTUtils.java
+++ src/main/java/com/example/fragrance_customizer_api/security/JWTUtils.java
@@ -1,7 +1,16 @@
 package com.example.fragrance_customizer_api.security;
 
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.logging.Logger;
 
 public class JWTUtils {
+    Logger logger = Logger.getLogger(JWTUtils.class.getName());
+    @Value("${jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${jwt-expiration-ms}")
+    private int jwtExpMs;
 
 
 }
