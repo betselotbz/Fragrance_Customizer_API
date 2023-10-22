5dfc03aa709356e28db2b1172efa9d9c3ee6737c
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-21 19:09:30 -0500

    Add method to obtain JWT key for user authentication

--- src/test/java/definitions/UserCartItemTestDefs.java
+++ src/test/java/definitions/UserCartItemTestDefs.java
@@ -3,10 +3,14 @@ package definitions;
 import com.example.fragrance_customizer_api.FragranceCustomizerApiApplication;
 import io.cucumber.java.en.Given;
 import io.cucumber.spring.CucumberContextConfiguration;
-import org.junit.platform.commons.logging.Logger;
-import org.junit.platform.commons.logging.LoggerFactory;
+import org.json.JSONException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = FragranceCustomizerApiApplication.class)
@@ -17,8 +21,24 @@ public class UserCartItemTestDefs {
 
     @LocalServerPort
     String port;
+    /**
+     * Retrieves a JWT (JSON Web Token) key by sending a POST request to the authentication endpoint.
+     * This method sets up the necessary HTTP request with user credentials and sends it to the
+     * authentication endpoint to obtain a JWT key for user authentication.
+     *
+     * @return The JWT key (token) received from the authentication response.
+     * @throws JSONException If there is an issue with JSON processing.
+     */
+
+    public static String getJWTKey(String port) throws JSONException {
+
+
+    }
+
 
     @Given("a user with ID {int} exists")
-    public void aUserWithIDExists(int arg0) {
+    public void aUserWithIDExists(int userId) {
+
+
     }
 }
