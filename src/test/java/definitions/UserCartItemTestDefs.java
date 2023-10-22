bba06504b71e1fd3a0c3952be1b4b06b9dac6616
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-21 19:12:40 -0500

    Set up RestAssured for API testing

--- src/test/java/definitions/UserCartItemTestDefs.java
+++ src/test/java/definitions/UserCartItemTestDefs.java
@@ -3,19 +3,19 @@ package definitions;
 import com.example.fragrance_customizer_api.FragranceCustomizerApiApplication;
 import io.cucumber.java.en.Given;
 import io.cucumber.spring.CucumberContextConfiguration;
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
 import org.json.JSONException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = FragranceCustomizerApiApplication.class)
 public class UserCartItemTestDefs {
 
+    private static final String BASE_URL = "http://localhost:";
     private final Logger logger = LoggerFactory.getLogger(UserCartItemTestDefs.class);
     private static final String Base_URL = "http://localhost:";
 
@@ -31,8 +31,10 @@ public class UserCartItemTestDefs {
      */
 
     public static String getJWTKey(String port) throws JSONException {
+        // Set the base URI and create a request
 
-
+        RestAssured.baseURI = BASE_URL;
+        RequestSpecification request = RestAssured.given();
     }
 
 
