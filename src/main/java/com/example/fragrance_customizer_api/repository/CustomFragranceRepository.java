a64c2a6396cf84a5b09ab1d55b4e948cfbb7b638
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-22 18:44:40 -0500

    Implement CustomFragrance Repository

---
+++ src/main/java/com/example/fragrance_customizer_api/repository/CustomFragranceRepository.java
@@ -0,0 +1,11 @@
+package com.example.fragrance_customizer_api.repository;
+
+import com.example.fragrance_customizer_api.model.CustomFragrance;
+import com.example.fragrance_customizer_api.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomFragranceRepository extends JpaRepository <CustomFragrance, Long> {
+    CustomFragrance findByNotes(String notes);
+}
