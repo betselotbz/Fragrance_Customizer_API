dbc9e4c5c6b1a0baf66d583ae860fa955daca91c
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-22 23:17:42 -0500

    Update methods in Ingredient Service and Repository

--- src/main/java/com/example/fragrance_customizer_api/repository/IngredientRepository.java
+++ src/main/java/com/example/fragrance_customizer_api/repository/IngredientRepository.java
@@ -1,10 +1,16 @@
 package com.example.fragrance_customizer_api.repository;
 
 import com.example.fragrance_customizer_api.model.Ingredient;
+import com.example.fragrance_customizer_api.model.Perfume;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
-    Ingredient findByName(String name);
+    Optional<Ingredient> findByName(String name);
+    Optional<Ingredient> findById(Long Id);
+    List<Ingredient> findAll();
 }
--- src/main/java/com/example/fragrance_customizer_api/service/IngredientService.java
+++ src/main/java/com/example/fragrance_customizer_api/service/IngredientService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class IngredientService {
@@ -15,10 +16,17 @@ public class IngredientService {
     private IngredientRepository ingredientRepository;
 
     @Autowired
-    public void setPerfumeRepository(IngredientRepository ingredientRepository) {
+    public void setIngredientRepository(IngredientRepository ingredientRepository) {
         this.ingredientRepository = ingredientRepository;
     }
     public IngredientService(List<Ingredient> ingredientList) {
         this.ingredientList = ingredientList;
+    }
+    public List<Ingredient> getAllIngredients() {return ingredientRepository.findAll();}
+    public Optional<Ingredient> getIngredientByName(String name) {
+        return ingredientRepository.findByName(name);
+    }
+    public Optional<Ingredient> getIngredientById(Long Id) {
+        return ingredientRepository.findById(Id);
     }
 }
