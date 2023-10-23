640d277e62388f9725bc6cd9381fe580a38148a0
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-23 01:33:52 -0500

    Added PerfumeController for API endpoints related to perfumes

---
+++ src/main/java/com/example/fragrance_customizer_api/controller/PerfumeController.java
@@ -0,0 +1,19 @@
+package com.example.fragrance_customizer_api.controller;
+
+import com.example.fragrance_customizer_api.service.PerfumeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/perfumes")
+public class PerfumeController {
+    private final PerfumeService perfumeService;
+
+    @Autowired
+    public PerfumeController(PerfumeService perfumeService) {
+        this.perfumeService = perfumeService;
+    }
+
+}
