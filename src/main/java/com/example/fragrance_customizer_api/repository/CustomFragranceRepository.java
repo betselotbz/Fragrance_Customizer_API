149a4e312f500ef64662ca83811a8065f0d11379
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-22 23:00:10 -0500

    Implement methods in CustomFragranceRepository

--- src/main/java/com/example/fragrance_customizer_api/repository/CustomFragranceRepository.java
+++ src/main/java/com/example/fragrance_customizer_api/repository/CustomFragranceRepository.java
@@ -1,10 +1,20 @@
 package com.example.fragrance_customizer_api.repository;
 
 import com.example.fragrance_customizer_api.model.CustomFragrance;
+import com.example.fragrance_customizer_api.model.Perfume;
+import com.example.fragrance_customizer_api.model.User;
+import com.example.fragrance_customizer_api.model.UserCartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CustomFragranceRepository extends JpaRepository <CustomFragrance, Long> {
-    CustomFragrance findByNotes(String notes);
+    Optional<CustomFragrance> findByFamily(String family);
+    Optional<CustomFragrance> findByNotes(String notes);
+    Optional<CustomFragrance> findById(Long Id);
+    List<CustomFragrance> findAll();
+
 }
