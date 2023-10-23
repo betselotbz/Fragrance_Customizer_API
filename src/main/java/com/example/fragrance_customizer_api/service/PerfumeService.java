91cb7d7743e87bc4c5fbefc2de8dcc1ce3c4ab58
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-22 22:00:50 -0500

    Implemented PerfumeService with a list of perfumes and a setter for PerfumeRepository injection

--- src/main/java/com/example/fragrance_customizer_api/service/PerfumeService.java
+++ src/main/java/com/example/fragrance_customizer_api/service/PerfumeService.java
@@ -1,18 +1,25 @@
 package com.example.fragrance_customizer_api.service;
 
 import com.example.fragrance_customizer_api.model.Perfume;
-import com.example.fragrance_customizer_api.model.User;
 import com.example.fragrance_customizer_api.repository.PerfumeRepository;
-import com.example.fragrance_customizer_api.repository.UserCartItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PerfumeService {
-    private final PerfumeRepository perfumeRepository;
+    private final List<Perfume> perfumeList;
+    private PerfumeRepository perfumeRepository;
 
     @Autowired
-    public PerfumeService(PerfumeRepository perfumeRepository) {
+    public void setPerfumeRepository(PerfumeRepository perfumeRepository) {
         this.perfumeRepository = perfumeRepository;
     }
+    public PerfumeService(List<Perfume> perfumeList) {this.perfumeList = perfumeList;}
+    
+    
+    
+    
 }
+
