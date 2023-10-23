3ebbd50685dfe7b0d3e2698df40ad4a058262cd6
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-22 19:05:56 -0500

    Inject UserCartItemRepository into UserCartItemService

--- src/main/java/com/example/fragrance_customizer_api/service/UserCartItemService.java
+++ src/main/java/com/example/fragrance_customizer_api/service/UserCartItemService.java
@@ -1,8 +1,14 @@
 package com.example.fragrance_customizer_api.service;
 
+import com.example.fragrance_customizer_api.repository.UserCartItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserCartItemService {
+    private UserCartItemRepository userCartItemRepository;
+
+    @Autowired
+    public void UserCartItemRepository(UserCartItemRepository userCartItemRepository) {this.userCartItemRepository = userCartItemRepository;}
 
 }
