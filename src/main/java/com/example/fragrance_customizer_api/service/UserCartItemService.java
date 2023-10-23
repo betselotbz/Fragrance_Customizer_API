d07216793b868044707c028a6a1c3cf246c28fd1
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-22 19:29:55 -0500

    Add getAllItems() method in UserCartItemService Class

--- src/main/java/com/example/fragrance_customizer_api/service/UserCartItemService.java
+++ src/main/java/com/example/fragrance_customizer_api/service/UserCartItemService.java
@@ -1,11 +1,14 @@
 package com.example.fragrance_customizer_api.service;
 
 import com.example.fragrance_customizer_api.model.User;
+import com.example.fragrance_customizer_api.model.UserCartItem;
 import com.example.fragrance_customizer_api.repository.UserCartItemRepository;
 import com.example.fragrance_customizer_api.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserCartItemService {
@@ -22,4 +25,8 @@ public class UserCartItemService {
         return myUserDetails.getUser();
 
     }
+    public List<UserCartItem> getAllItems() {
+        return userCartItemRepository.findAll();
+    }
+
 }
