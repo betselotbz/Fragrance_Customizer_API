69dd643990db8185ef9f4ef06e8a19bc455e3b3b
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-23 03:15:57 -0500

    Initialized IngredientController

--- src/main/java/com/example/fragrance_customizer_api/controller/IngredientController.java
+++ src/main/java/com/example/fragrance_customizer_api/controller/IngredientController.java
@@ -1,10 +1,16 @@
 package com.example.fragrance_customizer_api.controller;
 
+import com.example.fragrance_customizer_api.service.IngredientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/ingredients")
 public class IngredientController {
+    private final IngredientService ingredientService;
+
+    @Autowired
+    public IngredientController(IngredientService ingredientService) { this.ingredientService = ingredientService; }
 
 }
