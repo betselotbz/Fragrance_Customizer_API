0492b228b2347e3f3980c132e82eac366b0603dc
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-22 22:37:52 -0500

    Initialize CustomFragranceService with a List of CustomFragrance

--- src/main/java/com/example/fragrance_customizer_api/service/CustomFragranceService.java
+++ src/main/java/com/example/fragrance_customizer_api/service/CustomFragranceService.java
@@ -1,8 +1,17 @@
 package com.example.fragrance_customizer_api.service;
 
+import com.example.fragrance_customizer_api.model.CustomFragrance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CustomFragranceService {
+    private final List<CustomFragrance> customFragranceList;
 
+    @Autowired
+    public CustomFragranceService(List<CustomFragrance> customFragranceList) {
+        this.customFragranceList = customFragranceList;
+    }
 }
