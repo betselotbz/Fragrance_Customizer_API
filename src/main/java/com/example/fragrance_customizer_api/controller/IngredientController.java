343c90fc9ac73628899d2341c0db6a2360ac207c
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-23 03:13:35 -0500

    Create IngredientController Class

---
+++ src/main/java/com/example/fragrance_customizer_api/controller/IngredientController.java
@@ -0,0 +1,10 @@
+package com.example.fragrance_customizer_api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/ingredients")
+public class IngredientController {
+
+}
