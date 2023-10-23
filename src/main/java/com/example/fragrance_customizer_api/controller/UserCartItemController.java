44a1b80966558bfcdba0934b46c18475be96fee9
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-22 19:02:08 -0500

    Create UserCartItemService Class

--- src/main/java/com/example/fragrance_customizer_api/controller/UserCartItemController.java
+++ src/main/java/com/example/fragrance_customizer_api/controller/UserCartItemController.java
@@ -7,4 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/user-cart-items")
 public class UserCartItemController {
 
+
 }
---
+++ src/main/java/com/example/fragrance_customizer_api/service/UserCartItemService.java
@@ -0,0 +1,8 @@
+package com.example.fragrance_customizer_api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserCartItemService {
+
+}
