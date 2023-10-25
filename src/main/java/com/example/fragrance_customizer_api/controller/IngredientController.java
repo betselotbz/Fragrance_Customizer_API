f880b3e7339230bcf1170e221515821ca13d96b5
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-25 09:52:40 -0500

    Update Ingredient by adding Post, Put and Delete methods

--- src/main/java/com/example/fragrance_customizer_api/controller/IngredientController.java
+++ src/main/java/com/example/fragrance_customizer_api/controller/IngredientController.java
@@ -1,19 +1,13 @@
 package com.example.fragrance_customizer_api.controller;
 
 import com.example.fragrance_customizer_api.model.Ingredient;
-import com.example.fragrance_customizer_api.model.Perfume;
 import com.example.fragrance_customizer_api.service.IngredientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/api/ingredients")
@@ -26,48 +20,35 @@ public class IngredientController {
     }
 
     @GetMapping("/")
-    public ResponseEntity<?> getAllIngredients() {
-        List<Ingredient> ingredientList = ingredientService.getAllIngredients();
-        HashMap<String, Object> message = new HashMap<>();
-
-        if (ingredientList.isEmpty()) {
-            message.put("message", "Cannot find any perfumes.");
-            return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
-        } else {
-            message.put("message", "Success");
-            message.put("data", ingredientList);
-            return new ResponseEntity<>(message, HttpStatus.OK);
+    public List<Ingredient> getAllIngredients(@RequestParam(value = "name", required = false) String name) {
+        if (name == null){
+            return ingredientService.getAllIngredients();
         }
+        return ingredientService.getAllIngredientsByName(name);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getIngredientById(@PathVariable Long id) {
-        Optional<Ingredient> optionalIngredient = ingredientService.getIngredientById(id);
-        HashMap<String, Object> message = new HashMap<>();
-        if (optionalIngredient.isPresent()) {
-            Ingredient ingredient = optionalIngredient.get();
-            message.put("message", "Success");
-            message.put("data", ingredient);
-            return new ResponseEntity<>(message, HttpStatus.OK);
-        } else {
-            message.put("message", "Cannot find any Ingredient with the given ID.");
-            return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
-        }
+    public ResponseEntity<Ingredient> getIngredientById(@PathVariable Long id) {
+        return ingredientService.getIngredientById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
-    @GetMapping("/name/{name}")
-    public ResponseEntity<?> getIngredientByName(@PathVariable String name) {
-            Optional<Ingredient> optionalIngredient = ingredientService.getIngredientByName(name);
-            HashMap<String, Object> message = new HashMap<>();
-            if (optionalIngredient.isPresent()) {
-                Ingredient ingredient = optionalIngredient.get();
-                message.put("message", "Success");
-                message.put("data", ingredient);
-                return new ResponseEntity<>(message, HttpStatus.OK);
-            } else {
-                message.put("message", "Cannot find any Ingredient with the given name.");
-                return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
-            }
-        }
+    @PostMapping
+    public Ingredient createIngredient(@RequestBody Ingredient ingredient) {
+        return ingredientService.createIngredient(ingredient);
+    }
+
+    @PutMapping("/{id}")
+    public Ingredient updateIngredient(@PathVariable Long id, @RequestBody Ingredient ingredient) {
+        ingredient.setId(id);
+        return ingredientService.updateIngredient(ingredient);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteIngredient(@PathVariable Long id) {
+        ingredientService.deleteIngredient(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
