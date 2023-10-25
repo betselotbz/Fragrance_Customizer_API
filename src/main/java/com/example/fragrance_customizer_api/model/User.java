f149ec914fa1eb0bdcffea5362f38bbbec9a5af3
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-25 09:58:18 -0500

    Update User Model

--- src/main/java/com/example/fragrance_customizer_api/model/User.java
+++ src/main/java/com/example/fragrance_customizer_api/model/User.java
@@ -1,12 +1,10 @@
 package com.example.fragrance_customizer_api.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.hibernate.annotations.LazyCollection;
-import org.hibernate.annotations.LazyCollectionOption;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+
 /**
  * Entity class representing a user in the application.
  */
@@ -19,22 +17,28 @@ public class User {
     private Long id;
     @Column
     private String name;
+
     @Column(unique = true) //No two records in the table can have the same emailAddress.
     private String emailAddress;
+
     @Column
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) // ensure that the password is not accidentally exposed in the JSON representation of the object
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    // ensure that the password is not accidentally exposed in the JSON representation of the object
     private String password;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    @LazyCollection(LazyCollectionOption.FALSE)
-    private List<UserCartItem> userCartItems = new ArrayList<>();
+    @Column // user type can be admin or guest
+    private String type;
+
+    @OneToMany(mappedBy = "user")
+    private Collection<UserCartItem> userCartItems;
+
 
     /**
      * Default constructor needed for JPA.
      */
     public User() {
-
     }
+
     /**
      * Constructor to initialize a User with specific values.
      *
@@ -42,13 +46,16 @@ public class User {
      * @param name         The name of the user.
      * @param emailAddress The email address of the user.
      * @param password     The password of the user.
+     * @param type
      */
-    public User(Long id, String name, String emailAddress, String password) {
+    public User(Long id, String name, String emailAddress, String password, String type) {
         this.id = id;
         this.name = name;
-        this.emailAddress=emailAddress;
-        this.password=password;
+        this.emailAddress = emailAddress;
+        this.password = password;
+        this.type = type;
     }
+
     /**
      * Get the ID of the user.
      *
@@ -57,6 +64,7 @@ public class User {
     public Long getId() {
         return id;
     }
+
     /**
      * Set the ID of the user.
      *
@@ -65,6 +73,7 @@ public class User {
     public void setId(Long id) {
         this.id = id;
     }
+
     /**
      * Get the name of the user.
      *
@@ -73,6 +82,7 @@ public class User {
     public String getName() {
         return name;
     }
+
     /**
      * Set the name of the user.
      *
@@ -81,6 +91,7 @@ public class User {
     public void setName(String name) {
         this.name = name;
     }
+
     /**
      * Get the email address of the user.
      *
@@ -89,6 +100,7 @@ public class User {
     public String getEmailAddress() {
         return emailAddress;
     }
+
     /**
      * Set the email address of the user.
      *
@@ -97,6 +109,7 @@ public class User {
     public void setEmailAddress(String emailAddress) {
         this.emailAddress = emailAddress;
     }
+
     /**
      * Get the password of the user.
      *
@@ -105,6 +118,7 @@ public class User {
     public String getPassword() {
         return password;
     }
+
     /**
      * Set the password of the user.
      *
@@ -113,25 +127,26 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
-    /**
-     * Get the list of cart items associated with the user.
-     *
-     * @return The list of cart items associated with the user.
-     */
-    public List<UserCartItem> getUserCartItems() {
+
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    public Collection<UserCartItem> getUserCartItems() {
         return userCartItems;
     }
-    /**
-     * Set the list of cart items associated with the user.
-     *
-     * @param userCartItems The list of cart items to set.
-     */
-    public void setUserCartItems(List<UserCartItem> userCartItems) {
+
+    public void setUserCartItems(Collection<UserCartItem> userCartItems) {
         this.userCartItems = userCartItems;
     }
+
     /**
      * Returns a string representation of the User object.
-     *
+     * <p>
      * This implementation includes the user's id, name, email address, and password.
      *
      * @return A string representation of the User.
@@ -143,6 +158,7 @@ public class User {
                 ", name='" + name + '\'' +
                 ", emailAddress='" + emailAddress + '\'' +
                 ", password='" + password + '\'' +
+                ", type='" + type + '\'' +
                 '}';
     }
 }
