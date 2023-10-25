6b138a1f915446209f651f4fa5a6ecdaff9d1499
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-25 17:00:13 -0500

    update corsconfig

--- src/main/java/com/example/fragrance_customizer_api/security/CorsConfig.java
+++ src/main/java/com/example/fragrance_customizer_api/security/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("*")
                         .exposedHeaders("*");
             }
         };
