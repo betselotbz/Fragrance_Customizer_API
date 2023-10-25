b72a3130e5c3cc1d5155da3c4540ad81627cde97
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-25 10:07:25 -0500

    Add CorsConfig Class

---
+++ src/main/java/com/example/fragrance_customizer_api/CorsConfig/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.example.fragrance_customizer_api.CorsConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class CorsConfig {
+    public CorsConfig() {
+        System.out.println("CorsConfigurer is being configured.");
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowedOrigins("http://localhost:4200")
+                        .exposedHeaders("*");
+            }
+        };
+    }
+}
